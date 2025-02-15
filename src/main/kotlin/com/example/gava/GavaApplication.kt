0ca797ac7cee1c20b8a8ed62743be0e0b8648ca7
mong3125 <mong3125@naver.com>
2025-02-16 04:40:12 +0900

    config 어노테이션 이동

--- src/main/kotlin/com/example/gava/GavaApplication.kt
+++ src/main/kotlin/com/example/gava/GavaApplication.kt
@@ -2,10 +2,8 @@ package com.example.gava
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 
 @SpringBootApplication
-@EnableMethodSecurity
 class GavaApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/gava/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/gava/security/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -22,6 +23,7 @@ import org.springframework.web.filter.CorsFilter
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter
 ) {
